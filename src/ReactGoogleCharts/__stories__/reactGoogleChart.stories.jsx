ca98eaef14b766e8ffc65f7d7c5526e6bf370772
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-01-18 17:08:06 +0000

    Add ReactGoogleChart stories

---
+++ src/ReactGoogleCharts/__stories__/reactGoogleChart.stories.jsx
@@ -0,0 +1,40 @@
+import { Chart } from "react-google-charts"
+import React from "react"
+import { storiesOf } from '@storybook/react'
+
+const stageData = [
+    ['Stage', 'Value'],
+    ['Prospect', 7],
+    ['Assigned', 2],
+    ['Active', 5],
+    ['Verify win', 2],
+    ['Won', 2]
+  ]
+
+storiesOf('ReactGoogleCharts', module)
+  .add('Default', () => (
+    <Chart
+      width={'800px'}
+      height={'400px'}
+      chartType="PieChart"
+      data={stageData}
+      options={{
+          title: 'My projects'
+      }}
+      rootProps={{ 'data-testid': '1'}}
+      legendToggle
+    />
+  ))
+  .add('Donut', () => (
+    <Chart
+      width={'800px'}
+      height={'400px'}
+      chartType="PieChart"
+      data={stageData}
+      options={{
+          title: 'My projects',
+          pieHole: 0.4,
+      }}
+      rootProps={{ 'data-testid': '3'}}
+    />
+  ))
