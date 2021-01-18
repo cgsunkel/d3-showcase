1e08983d1d54b5c0426ecddbe0d0e18bbf8faf87
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-01-18 16:46:05 +0000

    Add MinimalPieChart

--- src/ReactMinimalPieChart/__stories__/minimalPieChart.stories.jsx
+++ src/ReactMinimalPieChart/__stories__/minimalPieChart.stories.jsx
@@ -1,25 +1,25 @@
-import MinimalPieChart from '../index'
+import { PieChart } from 'react-minimal-pie-chart'
 import { storiesOf } from '@storybook/react'
 
-import readme from '../readme.md'
-
-const data = [
-  { title: 'Prospect', value: 7, color: '#E38627' },
-  { title: 'Assigned', value: 2, color: '#C13C37' },
-  { title: 'Active', value: 5, color: '##777fff' },
-  { title: 'Verify win', value: 2, color: '#8800ff' },
-  { title: 'Won', value: 2, color: '#990000' },
+const stageData = [
+  { title: 'Prospect', value: 7, color: '#0088FE' },
+  { title: 'Assigned', value: 2, color: '#00C49F' },
+  { title: 'Active', value: 5, color: '#FFBB28' },
+  { title: 'Verify win', value: 2, color: '#FF8042' },
+  { title: 'Won', value: 2, color: '#8b4513' },
 ]
 
-const viewBoxSize = [100, 100]
-
 storiesOf('MinimalPieChart', module)
-  .addParameters({
-    options: { theme: undefined },
-    readme: {
-      sidebar: readme,
-    },
-  })
   .add('Default', () => (
-    <MinimalPieChart data={data} viewBoxSize={viewBoxSize} />
+    <PieChart
+      data={stageData}
+      label={({ dataEntry }) => dataEntry.value}
+      labelStyle={(index) => ({
+        fill: stageData[index].color,
+        fontSize: '5px',
+        fontFamily: 'sans-serif',
+      })}
+      radius={42}
+      labelPosition={112}
+    />
   ))
