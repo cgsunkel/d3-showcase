8e33137b5086dd767642c3e1ce928ec59a03b4b7
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-01-19 15:23:28 +0000

    Remove unused colour import

--- src/ReactGoogleCharts/__stories__/reactGoogleChart.stories.jsx
+++ src/ReactGoogleCharts/__stories__/reactGoogleChart.stories.jsx
@@ -1,41 +1,40 @@
-import React from "react"
-import { Chart } from "react-google-charts"
-import { storiesOf } from '@storybook/react'
-import { BLUE, YELLOW, GREEN, TURQUOISE, GRASS_GREEN } from 'govuk-colours'
+import React from "react";
+import { Chart } from "react-google-charts";
+import { storiesOf } from "@storybook/react";
 
 const stageData = [
-    ['Stage', 'Value'],
-    ['Prospect', 7],
-    ['Assigned', 2],
-    ['Active', 5],
-    ['Verify win', 2],
-    ['Won', 2]
-  ]
+  ["Stage", "Value"],
+  ["Prospect", 7],
+  ["Assigned", 2],
+  ["Active", 5],
+  ["Verify win", 2],
+  ["Won", 2],
+];
 
-storiesOf('ReactGoogleCharts', module)
-  .add('Default', () => (
+storiesOf("ReactGoogleCharts", module)
+  .add("Default", () => (
     <Chart
-      width={'800px'}
-      height={'400px'}
+      width={"800px"}
+      height={"400px"}
       chartType="PieChart"
       data={stageData}
       options={{
-          title: 'My projects',
+        title: "My projects",
       }}
-      rootProps={{ 'data-testid': '1'}}
+      rootProps={{ "data-testid": "1" }}
       legendToggle
     />
   ))
-  .add('Donut', () => (
+  .add("Donut", () => (
     <Chart
-      width={'800px'}
-      height={'400px'}
+      width={"800px"}
+      height={"400px"}
       chartType="PieChart"
       data={stageData}
       options={{
-          title: 'My projects',
-          pieHole: 0.4,
+        title: "My projects",
+        pieHole: 0.4,
       }}
-      rootProps={{ 'data-testid': '3'}}
+      rootProps={{ "data-testid": "3" }}
     />
-  ))
+  ));
