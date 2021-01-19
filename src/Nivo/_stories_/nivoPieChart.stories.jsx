477315f853050dfb3574ee598262bc4e3ec677ff
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2021-01-19 15:14:54 +0000

    Add govuk colours to Nivo charts

--- src/Nivo/_stories_/nivoPieChart.stories.jsx
+++ src/Nivo/_stories_/nivoPieChart.stories.jsx
@@ -1,70 +1,73 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { withKnobs, boolean } from '@storybook/addon-knobs'
-import { Pie } from '@nivo/pie'
-import { stageData } from '../data'
-//import { BLUE, YELLOW, GREEN, TURQUOISE, GRASS_GREEN } from 'govuk-colours'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { Pie } from "@nivo/pie";
+import { stageData } from "../data";
+import { BLUE, YELLOW, GREEN, TURQUOISE, GRASS_GREEN } from "govuk-colours";
+
+const govColours = [BLUE, YELLOW, GREEN, TURQUOISE, GRASS_GREEN];
 
 const commonProperties = {
   width: 900,
   height: 500,
   margin: { top: 80, right: 120, bottom: 80, left: 120 },
   data: stageData,
+  colors: govColours,
   animate: true,
-}
+};
 
 const legends = [
   {
-    anchor: 'bottom',
-    direction: 'row',
+    anchor: "bottom",
+    direction: "row",
     translateY: 56,
     itemWidth: 100,
     itemHeight: 18,
-    itemTextColor: '#999',
+    itemTextColor: "#999",
     symbolSize: 18,
-    symbolShape: 'circle',
+    symbolShape: "circle",
     effects: [
       {
-        on: 'hover',
+        on: "hover",
         style: {
-          itemTextColor: '#000',
+          itemTextColor: "#000",
         },
       },
     ],
   },
-]
+];
 
-const stories = storiesOf('Nivo', module)
+const stories = storiesOf("Nivo", module);
 
-stories.addDecorator(withKnobs)
+stories.addDecorator(withKnobs);
 
-stories.add('default', () => (
+stories.add("default", () => (
   <Pie
     {...commonProperties}
-    legends={boolean('legends', false) ? legends : []}
+    legends={boolean("legends", false) ? legends : []}
   />
-))
+));
 
-stories.add('donut', () => <Pie {...commonProperties} innerRadius={0.6} />)
+stories.add("donut", () => <Pie {...commonProperties} innerRadius={0.6} />);
 
-stories.add('fancy slices', () => (
+stories.add("fancy slices", () => (
   <Pie
     {...commonProperties}
     innerRadius={0.6}
     padAngle={0.5}
     cornerRadius={5}
     radialLabelsLinkColor={{
-      from: 'color',
+      from: "color",
     }}
     radialLabelsLinkStrokeWidth={3}
     radialLabelsTextColor={{
-      from: 'color',
-      modifiers: [['darker', 1.2]],
+      from: "color",
+      modifiers: [["darker", 1.2]],
     }}
   />
-))
+));
 
-stories.add('custom radial label', () => (
+stories.add("custom radial label", () => (
   <Pie
     {...commonProperties}
     innerRadius={0.6}
@@ -72,22 +75,22 @@ stories.add('custom radial label', () => (
     cornerRadius={5}
     radialLabel={(d) => `${d.id}: ${d.value}`}
     radialLabelsLinkColor={{
-      from: 'color',
+      from: "color",
     }}
     radialLabelsLinkStrokeWidth={3}
     radialLabelsTextColor={{
-      from: 'color',
-      modifiers: [['darker', 1.2]],
+      from: "color",
+      modifiers: [["darker", 1.2]],
     }}
     enableSliceLabels={false}
   />
-))
+));
 
 const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
-  let total = 0
+  let total = 0;
   dataWithArc.forEach((datum) => {
-    total += datum.value
-  })
+    total += datum.value;
+  });
 
   return (
     <text
@@ -96,43 +99,43 @@ const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
       textAnchor="middle"
       dominantBaseline="central"
       style={{
-        fontSize: '52px',
-        fontWeight: '600',
+        fontSize: "52px",
+        fontWeight: "600",
       }}
     >
       {total}
     </text>
-  )
-}
+  );
+};
 
-stories.add('adding a metric in the center using a custom layer', () => (
+stories.add("adding a metric in the center using a custom layer", () => (
   <Pie
     {...commonProperties}
     innerRadius={0.8}
     enableSliceLabels={false}
     radialLabel={(d) => `${d.id} (${d.formattedValue})`}
     layers={[
-      'slices',
-      'sliceLabels',
-      'radialLabels',
-      'legends',
+      "slices",
+      "sliceLabels",
+      "radialLabels",
+      "legends",
       CenteredMetric,
     ]}
   />
-))
+));
 
-stories.add('formatted tooltip values', () => (
+stories.add("formatted tooltip values", () => (
   <Pie
     {...commonProperties}
     tooltipFormat={(value) =>
-      `${Number(value).toLocaleString('ru-RU', {
+      `${Number(value).toLocaleString("ru-RU", {
         minimumFractionDigits: 2,
       })} ₽`
     }
   />
-))
+));
 
-stories.add('custom tooltip', () => (
+stories.add("custom tooltip", () => (
   <Pie
     {...commonProperties}
     tooltip={({ id, value, color }) => (
@@ -143,21 +146,21 @@ stories.add('custom tooltip', () => (
     theme={{
       tooltip: {
         container: {
-          background: '#333',
+          background: "#333",
         },
       },
     }}
   />
-))
+));
 
-stories.add('enter/leave (check console)', () => (
+stories.add("enter/leave (check console)", () => (
   <Pie
     {...commonProperties}
     onMouseEnter={(data, e) => {
-      console.log({ is: 'mouseenter', data, event: e })
+      console.log({ is: "mouseenter", data, event: e });
     }}
     onMouseLeave={(data, e) => {
-      console.log({ is: 'mouseleave', data, event: e })
+      console.log({ is: "mouseleave", data, event: e });
     }}
   />
-))
+));
